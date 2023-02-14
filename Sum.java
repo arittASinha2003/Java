300c14a70fb332b5a0014e81f83cb151370a9714
Aritt Sinha <93367827+arittASinha2003@users.noreply.github.com>
2023-02-14 09:09:26 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+  public static void main(String args[]) {
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+    int Sum = a + b;
+
+    System.out.println("Sum: " + Sum);
+  }
+}
