bfe67fc53a18fa5205204986ff51bbccd2c1ba36
Aritt Sinha <93367827+arittASinha2003@users.noreply.github.com>
2023-03-16 15:30:55 +0530

    Add files via upload

---
+++ Autoboxing.java
@@ -0,0 +1,9 @@
+class Autoboxing
+{
+	public static void main (String args[])
+	{
+		int a = 10;
+		Integer inobj = a;
+		System.out.println (inobj);
+	}
+}
---
+++ Unboxing.java
@@ -0,0 +1,10 @@
+class Unboxing
+{
+	public static void main (String args[])
+	{
+		int a = 10;
+		Integer inobj = a;
+		int x = inobj;
+		System.out.println (x);
+	}
+}
